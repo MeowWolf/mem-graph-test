6ca3f87d79734c968f047eafd6814f17685771e3
Sam <samjones@meowwolf.com>
2021-05-06 15:34:04 -0600

    clean it up

--- src/App.tsx
+++ src/App.tsx
@@ -7,78 +7,10 @@ import testData from './testData'
 import * as THREE from 'three'
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
 
-const HERO_MEM = {
-  id: 'mem_hero_01a.png',
-  //memory: 'memory01',
-  name: 'Hero 01 A',
-  color: '#2BACE2 ',
-  visible: true,
-}
-
-const EMPTY_NODE = {
-  id: 'empty',
-}
-
-// if no links then create mem tendrils
-
-// build links then nodes for now. Nodes are not dynamic
-
-function initialHeroMemLinks(heroMem: any) {
-  const links = []
-  for (let i = 0; i < 8; i++) {
-    links.push({
-      source: heroMem.id,
-      target: `${heroMem.id}-empty${i}`,
-      //length
-      value: 1,
-      color: '#2BACE2 ',
-      curve: 0.5,
-      rotation: -Math.PI * (i / 6),
-    })
-  }
-
-  return links
-}
-
-function emptyNodes(heroMem: any) {
-  const nodes = []
-  for (let i = 0; i < 8; i++) {
-    nodes.push({
-      id: `${heroMem.id}-empty${i}`,
-      name: 'empty',
-      visible: false,
-    })
-  }
-
-  return nodes
-}
-
-const NEW_HERO_MEM = {
-  nodes: [HERO_MEM, ...emptyNodes(HERO_MEM)],
-  links: initialHeroMemLinks(HERO_MEM),
-}
-
-const test = {
-  nodes: [...NEW_HERO_MEM.nodes],
-  links: [...NEW_HERO_MEM.links],
-}
-
 const App: FunctionComponent = () => {
-  // if (
-  //   !process.env.REACT_APP_MESSAGE_BUS_LOCATION ||
-  //   !process.env.REACT_APP_PLATFORM_LOCATION ||
-  //   !process.env.REACT_APP_CORE_API_PORT ||
-  //   !process.env.REACT_APP_MESSAGE_BUS_PORT ||
-  //   !process.env.REACT_APP_PLATFORM_TOKEN ||
-  //   !process.env.REACT_APP_CORE_API_PORT
-  // )
-  //   return null
-
-  // Might need a useeffect or layout hook
   const graphParent = useRef<HTMLDivElement>(null)
   // @ts-expect-error // shhh
   let Graph
-  let linkForce
   let graphControls
 
   let first = true
@@ -100,7 +32,6 @@ const App: FunctionComponent = () => {
       Graph = ForceGraph()(graphParent.current)
         .graphData(testData)
         .width(width * 0.8)
-
         .linkColor('color')
         .linkWidth(1)
         .linkResolution(10)
@@ -163,15 +94,6 @@ const App: FunctionComponent = () => {
   }, [graphParent.current])
 
   return (
-    // <Platform
-    //   messageBusPort={parseInt(process.env.REACT_APP_MESSAGE_BUS_PORT)}
-    //   messageBusLocation={process.env.REACT_APP_MESSAGE_BUS_LOCATION}
-    //   platformToken={process.env.REACT_APP_PLATFORM_TOKEN}
-    //   platformIP={process.env.REACT_APP_PLATFORM_LOCATION}
-    //   coreAPIPort={parseInt(process.env.REACT_APP_CORE_API_PORT)}
-    // >
-    //   <Tester />
-    // </Platform>
     <div className="container">
       <div className="rail"></div>
       <div id="3d-graph" className="graph" ref={graphParent}></div>
@@ -186,13 +108,5 @@ const App: FunctionComponent = () => {
     </div>
   )
 }
-
-// const Tester: FunctionComponent = () => {
-//   const config = useContext(ConfigContext)
-//   useEffect(() => {
-//     console.log(config)
-//   }, [config])
-//   return <p>Loaded Config. Things seem good to go.</p>
-// }
 
 export default App
